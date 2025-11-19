d17e3f7b10293b5ca7ab0504598a5d715bb6cbfd
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-18 19:18:36 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -2,3 +2,18 @@ CREATE TABLE usuarios (
   id SERIAL PRIMARY KEY,
   data JSONB
 );
+
+CREATE EXTENSION IF NOT EXISTS hstore;
+
+CREATE TABLE productos_jsonb (
+  id SERIAL PRIMARY KEY,
+  nombre TEXT,
+  specs JSONB
+);
+
+CREATE TABLE productos_hstore (
+  id SERIAL PRIMARY KEY,
+  nombre TEXT,
+  atributos HSTORE,
+  precio NUMERIC
+);
